51a4c7fb05c859114eb4926752948c93aa0b2914
Rebecca <beckylum0214@gmail.com>
2019-01-16 19:24:31 +0800

    starting convolution work

---
+++ Convolution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by becky on 16/01/2019.
+//
+
+#include "Convolution.h"
---
+++ Convolution.h
@@ -0,0 +1,14 @@
+//
+// Created by becky on 16/01/2019.
+//
+
+#ifndef TEMPMLPNN_CONVOLUTION_H
+#define TEMPMLPNN_CONVOLUTION_H
+
+
+class Convolution {
+
+};
+
+
+#endif //TEMPMLPNN_CONVOLUTION_H
